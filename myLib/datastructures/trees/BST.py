97ebeceb52ee16a665235fc3e66f9a1f1759322c
Rodmichael <rodmichael.umapas@ucalgary.ca>
2023-04-09 17:35:55 -0600

    finish BST

--- myLib/datastructures/trees/BST.py
+++ myLib/datastructures/trees/BST.py
@@ -120,5 +120,30 @@ class BST:
             print(node)
 
 
-##need printBF()
+    def printBF(root):
+        if root is None:
+            return
+
+        queue = [root]
+        next_level_count = 0
+        current_level_count = 1
+
+        while queue:
+            node = queue.pop(0)
+            current_level_count -= 1
+
+            print(node.data, end=" ")
+
+            if node.left:
+                queue.append(node.left)
+                next_level_count += 1
+
+            if node.right:
+                queue.append(node.right)
+                next_level_count += 1
+
+            if current_level_count == 0:
+                print()
+                current_level_count = next_level_count
+                next_level_count = 0
                
